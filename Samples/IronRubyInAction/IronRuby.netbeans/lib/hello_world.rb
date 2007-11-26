8dfa779023f37f7fb8b4c094400b49c336840665
Ivan Porto Carrero <ivan@flanders.co.nz>
2007-11-26 10:09:32 +0000

    git-svn-id: https://svn.koolkraft.net/ironruby_book/trunk@11 a441d6a3-5e39-0410-b140-cfe3d07c745c

---
+++ Samples/IronRubyInAction/IronRuby.netbeans/lib/hello_world.rb
@@ -0,0 +1,15 @@
+# 
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+ 
+
+class HelloWorld
+   
+  def initialize
+    @output = "Hello world !!!"
+  end
+  
+  def print
+    @output
+  end
+end
