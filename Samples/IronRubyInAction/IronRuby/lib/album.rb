b93cbb9fac75fda30fb5f7b4f211f95e8d399a19
Ivan Porto Carrero <ivan@flanders.co.nz>
2007-12-28 22:19:59 +0000

    git-svn-id: https://svn.koolkraft.net/ironruby_book/trunk@22 a441d6a3-5e39-0410-b140-cfe3d07c745c

---
+++ Samples/IronRubyInAction/IronRuby/lib/album.rb
@@ -0,0 +1,25 @@
+class Album
+	
+	def initialize(name, artist, songs)
+		@name = name
+		@artist = artist
+		@songs = songs
+	end
+	
+	def print
+		"Name: #@name\nArtist: #@artist\nSongs: #{@songs.join(', ')}"
+	end
+	
+end
+
+album = Album.new("Ruby Greatest Hits", "Various", ["Summer of 95", "Every character you type", "RubyEyed Girl"])
+
+puts "Album: "
+puts album.print
+
+# outputs the following:
+#
+# Album:
+# Name: Ruby Greatest Hits
+# Artist: Various
+# Songs: Summer of 95, Every character you type, RubyEyed Girl
