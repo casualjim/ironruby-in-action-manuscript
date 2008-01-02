310975e0dfb21b6ea3a25b68a559644bfeea0206
Ivan Porto Carrero <ivan@flanders.co.nz>
2008-01-02 00:31:37 +0000

    git-svn-id: https://svn.koolkraft.net/ironruby_book/trunk@28 a441d6a3-5e39-0410-b140-cfe3d07c745c

--- Samples/IronRubyInAction/CSharp/CSharp/Program.cs
+++ Samples/IronRubyInAction/CSharp/CSharp/Program.cs
@@ -7,46 +7,22 @@ namespace CSharp
 {
     class Program
     {
-static void Main(string[] args)
-{
-    List<object> printables = new List<object>{ new HelloWorld(), new Book("IronPython In Action") };
+        static void Main(string[] args)
+        {
+           
+            List<object> printables = new List<object> { new HelloWorld(), new Book("IronPython In Action") };
 
-    printables.ForEach(printable => Output(printable as IPrintable));
+            printables.ForEach(printable => Output(printable as IPrintable));
 
-    object hw = printables[0];
-    Console.WriteLine("hw does {0}implement IPrintable", (hw is IPrintable) ? string.Empty : "not ");
+            object hw = printables[0];
+            Console.WriteLine("hw does {0}implement IPrintable", (hw is IPrintable) ? string.Empty : "not ");
 
-}
+        }
 
-static void Output(IPrintable toOutput)
-{
-    Console.WriteLine(toOutput.Print());
-}
+        static void Output(IPrintable toOutput)
+        {
+            Console.WriteLine(toOutput.Print());
+        }
     }
 }
 
-/*
- *             if (args.Length == 0 || args[0] == "helloworld")
-            {
-                IPrintable printable = new HelloWorld();
-                Console.WriteLine(printable.Print());
-            }
-            else
-            {
-                switch (args[0])
-                {
-                    case "statictyping":
-                        StaticTyping.Demonstrate();
-                        break;
-                    case "typeinference":
-                        TypeInference.Demonstrate();
-                        break;
-                    case "emit":
-                        HelloWorldEmit.Demonstrate();
-                        break;
-                    case "lcg":
-                        HelloWorldLCG.Demonstrate();
-                        break;
-                }
-            }
-*/
