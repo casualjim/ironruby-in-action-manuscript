5c224748481a1469c7806d94dddabd58b95d2f37
Ivan Porto Carrero <ivan@flanders.co.nz>
2008-01-02 08:31:04 +0000

    git-svn-id: https://svn.koolkraft.net/ironruby_book/trunk@30 a441d6a3-5e39-0410-b140-cfe3d07c745c

--- Samples/IronRubyInAction/IronRuby/lib/album_list_with_iterator.rb
+++ Samples/IronRubyInAction/IronRuby/lib/album_list_with_iterator.rb
@@ -1,19 +1,5 @@
-collection = %w(Some words in a collection)
-
-#for iterator
-puts "With a for iterator:"
-for word in collection
- puts word
-end
-
-#with a block
-puts "\nWith a block iterator:"
-collection.each do |word|
-	print "#{word} "
-end
-print "\n"
-
 require 'album_list' 
+
 class AlbumList
 	
 	def each_in_range(range, &block)
@@ -32,16 +18,6 @@ list.each_in_range(0..1){|album| puts album}
 
 # Produces the following output:
 #
-# With a for iterator:
-# Some
-# words
-# in
-# a
-# collection
-# 
-# With a block iterator:
-# Some words in a collection
-# 
 # Custom iterator:
 # Ruby tuesday
 # Ruby is a diamond
