41407bf08110d4fb21d9deb88614256d40c23d43
Ivan Porto Carrero <ivan@flanders.co.nz>
2008-01-20 23:09:00 +0000

    git-svn-id: https://svn.koolkraft.net/ironruby_book/trunk@44 a441d6a3-5e39-0410-b140-cfe3d07c745c

--- Samples/IronRubyInAction/IronRuby/wpf/xaml_main.rb
+++ Samples/IronRubyInAction/IronRuby/wpf/xaml_main.rb
@@ -1,11 +1,13 @@
 p "loading components, please wait..."
 
+# get the necessary assemblies loaded for .NET
 require 'mscorlib'
 require 'System, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' 
 require 'System.Xml, Version=2.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089'
 require 'PresentationFramework, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'
 require 'PresentationCore, Version=3.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35'
 
+# include some namespaces for easy access
 include System
 include System::Windows
 include System::Windows::Controls
@@ -13,7 +15,8 @@ include System::Windows::Markup
 include System::Windows::Media
 include System::Windows::Media::Animation
 include System::Xml
- 
+
+# define an application
 class XamlApplication < Application
 
   def initialize    
@@ -22,6 +25,7 @@ class XamlApplication < Application
   
 end
 
+#run the application
 xaml_path = "Biffy.xaml"
 
 XamlApplication.new do
