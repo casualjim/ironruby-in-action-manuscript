6093fe2d9f4f355365343a3f34840273c70c368d
Ivan Porto Carrero <ivan@flanders.co.nz>
2008-02-20 21:51:52 +0000

    git-svn-id: https://svn.koolkraft.net/ironruby_book/trunk@62 a441d6a3-5e39-0410-b140-cfe3d07c745c

---
+++ Samples/IronRubyDataAccess/DataAccess.CsharpTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccess.LightSpeed;
+using Mindscape.LightSpeed;
+
+namespace DataAccess.CsharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Customer> customer = Repository.Find<Customer>();
+
+            customer.ToList().ForEach(c => Console.WriteLine(c.CompanyName));
+        }
+    }
+}
--- Samples/IronRubyDataAccess/DataAccess.LightSpeed/MiddleManager.cs
+++
@@ -1,9 +0,0 @@
-using Mindscape.LightSpeed;
-
-namespace DataAccess.LightSpeed
-{
-  [Discriminator(Attribute="ReportsToId", Value=2)]
-  public sealed class MiddleManager : Employee
-  {
-  }
-}
