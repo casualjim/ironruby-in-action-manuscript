d8502c23a7e95526b03878a0fcedb07bcf76b22f
Adam Burmister <adam.burmister@gmail.com>
2009-11-27 16:15:39 +0000

    Checking in high scores service

--- Samples/Chapter5_Silverlight_Gemtris/Gemtris/highscore_service/sinatra_highscores_service.rb
+++ Samples/Chapter5_Silverlight_Gemtris/Gemtris/highscore_service/sinatra_highscores_service.rb
@@ -1,10 +1,24 @@
 require 'rubygems'
 require 'sinatra'
+require 'rexml/document'
+include REXML # so we don't have to prefix everything
 
 class SinatraHighscoresService
   
-  get '/highscores'
-    
+  get '/highscores' do
+    scores_xml = (Document.new File.new 'scores.xml').root
+    scores_xml.to_s
+  end
+  
+  post '/highscores' do
+    file = File.new('scores.xml', 'r+')
+    scores_xml = Document.new(file)
+    scores = scores_xml.root
+    new_entry = scores << Element.new("score")
+    new_entry['player'] = params[:name]
+    new_entry.text = params[:score]
+    open('scores.xml', 'w'){ |f| scores_xml.write f }
+    scores_xml
   end
   
 end
